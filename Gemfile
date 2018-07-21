8fc1019eed6bee4a57803f04cbb325c6f404d234
GrzegorzBanasiak <grzegorzbanasiak95@gmail.com>
2018-07-21 11:18:45 +0200

    add haml and create root page

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap'
 gem 'jquery-rails'
+gem 'haml'
 
 group :development, :test do
   gem 'pry'
--- db/migrate/20180721082942_drop_posts_table.rb
+++ db/migrate/20180721082942_drop_posts_table.rb
@@ -1,8 +1,7 @@
 class DropPostsTable < ActiveRecord::Migration[5.2]
   def up
-      drop_table :posts
+    drop_table :posts
   end
-
 
   def down
     raise ActiveRecord::IrreversibleMigration
