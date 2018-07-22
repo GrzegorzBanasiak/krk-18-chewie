74ace8cc4cf226baac0014fd91669d4298ba0cd9
Monika <pogorzelska.monika@gmail.com>
2018-07-22 12:53:45 +0200

    count nutrition

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,31 @@ class Recipe < ApplicationRecord
   has_many :products, through: :recipe_ingredients
 
   ratyrate_rateable "rate"
+
+  def calories
+  	nutrition(:calories)
+  end
+
+  def carbohydrates
+  	nutrition(:carbohydrates)
+  end
+
+  def protein
+  	nutrition(:protein)
+  end
+
+  def fat
+  	nutrition(:fat)
+  end
+
+  private
+
+  def nutrition(name)
+  	amount = 0
+  	recipe_ingredients.each do |ingredient|
+  		rate = ingredient[:weight]/100
+  		amount += ingredient.product[name] * rate
+  	end
+  	amount
+  end
 end
