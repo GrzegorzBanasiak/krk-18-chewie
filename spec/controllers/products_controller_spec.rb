1f9fa8303c87dcc7d92f23a858fee70851d24699
Monika <pogorzelska.monika@gmail.com>
2018-07-22 10:01:37 +0200

    layout and links

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -15,19 +15,4 @@ RSpec.describe ProductsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #delete" do
-    it "returns http success" do
-      get :delete
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
