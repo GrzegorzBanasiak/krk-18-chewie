69dce379f6cabbdc8726a2d3939cc130bfabf4a7
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2018-07-22 13:39:37 +0200

    Uncomment ratyrate

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :products, through: :recipe_ingredients
 
-  # ratyrate_rateable "rate"
+  ratyrate_rateable "rate"
 
   def calories
     nutrition(:calories)
