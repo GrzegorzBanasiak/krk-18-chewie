4ad629a23278fb7f267bd34440939409b91749cf
GrzegorzBanasiak <grzegorzbanasiak95@gmail.com>
2018-07-22 11:11:41 +0200

    fixed bug with products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!,
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_product, only: [:edit, :show, :update, :delete]
 
   def index
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,10 @@
 class Product < ApplicationRecord
-<<<<<<< Updated upstream
   belongs_to :user
-=======
+
 
   def to_s
     name
   end
-  
->>>>>>> Stashed changes
+
+
 end
