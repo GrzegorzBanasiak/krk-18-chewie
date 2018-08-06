ce744649e7cb03add6608a3f526523c367b8cb35
GrzegorzBanasiak <grzegorzbanasiak95@gmail.com>
2018-08-06 22:42:42 +0200

    if product didn't found, show empty form

--- app/assets/javascripts/get_food_from_api.js
+++ app/assets/javascripts/get_food_from_api.js
@@ -16,22 +16,26 @@ $( document ).ready(function() {
 
         var filteredFoods = resultsFoods.filter(isAllParams);
 
-        filteredFoods.forEach( (food)=>{
-          $('#list-food').append("<li class='list-group-item' data-name="+ food.food.label + " data-carbohydrates="+ food.food.nutrients.CHOCDF +" data-protein="+ food.food.nutrients.PROCNT +"  data-fat="+ food.food.nutrients.FAT +" data-calories="+ food.food.nutrients.ENERC_KCAL+">"+food.food.label+" </li>");
-        })
+        if (filteredFoods == 0) {
+          alert("We didn't find :(, write your own products");
+        } else {
+          filteredFoods.forEach( (food)=>{
+            $('#list-food').append("<li class='list-group-item' data-name="+ food.food.label + " data-carbohydrates="+ food.food.nutrients.CHOCDF +" data-protein="+ food.food.nutrients.PROCNT +"  data-fat="+ food.food.nutrients.FAT +" data-calories="+ food.food.nutrients.ENERC_KCAL+">"+food.food.label+" </li>");
+          })
 
-        var list_food = $('#list-food li').on('click', (e)=>{
-          $('.hidden-inputs').css('display', 'block');
-          $($('#calories-input')[0]).val(parseFloat(e.target.dataset.calories).toFixed(2));
-          $($('#carbo-input')[0]).val(parseFloat(e.target.dataset.carbohydrates).toFixed(2));
-          $($('#protein-input')[0]).val(parseFloat(e.target.dataset.protein).toFixed(2));
-          $($('#fat-input')[0]).val(parseFloat(e.target.dataset.fat).toFixed(2));
-          $($('#food-name-input')[0]).val(e.target.dataset.name);
-          list_food.remove();
-        })
+          var list_food = $('#list-food li').on('click', (e)=>{
+            $($('#calories-input')[0]).val(parseFloat(e.target.dataset.calories).toFixed(2));
+            $($('#carbo-input')[0]).val(parseFloat(e.target.dataset.carbohydrates).toFixed(2));
+            $($('#protein-input')[0]).val(parseFloat(e.target.dataset.protein).toFixed(2));
+            $($('#fat-input')[0]).val(parseFloat(e.target.dataset.fat).toFixed(2));
+            $($('#food-name-input')[0]).val(e.target.dataset.name);
+            list_food.remove();
+          })
+        };
+        $('.hidden-inputs').css('display', 'block');
       },
       error: function(textStatus){
-        console.log(textStatus);
+        //console.log(textStatus);
         $('#list-food').append("<li class='list-group-item>Not found</li>");
       }
     });
