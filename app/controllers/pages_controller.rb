eaf7a3beb4c52ca46cf2b771eee2df445c3525ce
GrzegorzBanasiak <grzegorzbanasiak95@gmail.com>
2018-08-09 20:15:48 +0200

    add pages-panel site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def index; end
+  before_action :authenticate_user!, only: [:panel]
+
+  def index
+    if user_signed_in?
+      redirect_to pages_panel_path
+    end
+  end
+
+  def panel
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
   root 'pages#index'
+  get 'pages/panel'
   resources :recipes do
     post 'add_products', on: :member
   end
